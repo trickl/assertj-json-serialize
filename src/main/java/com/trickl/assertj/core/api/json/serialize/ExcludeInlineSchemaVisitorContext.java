998e4ba1606fa1a3d8ab66d34fe8e184d5bb641a
Tim Gee <writetotimgee@gmail.com>
2022-12-03 14:24:24 +0000

    Update libraries

--- src/main/java/com/trickl/assertj/core/api/json/serialize/ExcludeInlineSchemaVisitorContext.java
+++ src/main/java/com/trickl/assertj/core/api/json/serialize/ExcludeInlineSchemaVisitorContext.java
@@ -2,9 +2,7 @@ package com.trickl.assertj.core.api.json.serialize;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.module.jsonSchema.factories.VisitorContext;
-
 import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -21,8 +19,8 @@ public class ExcludeInlineSchemaVisitorContext extends VisitorContext {
 
   private boolean isInExcludedPackage(JavaType javaType) {
     String packageName = javaType.getRawClass().getPackage().getName();
-    return excludePackages.stream().anyMatch((exclude -> 
-      packageName.equals(exclude) || packageName.startsWith(exclude + ".")
-    ));
+    return excludePackages.stream()
+        .anyMatch(
+            (exclude -> packageName.equals(exclude) || packageName.startsWith(exclude + ".")));
   }
 }
